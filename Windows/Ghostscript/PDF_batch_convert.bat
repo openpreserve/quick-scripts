4e6785ef148f0bc54eba9fabf132869026a5c30e
Darren Dignam <darren.dignam@gmail.com>
2023-11-06 12:19:01 +0100

    Update PDF_batch_convert.bat
    Added a license to the file

--- Windows/Ghostscript/PDF_batch_convert.bat
+++ Windows/Ghostscript/PDF_batch_convert.bat
@@ -1,5 +1,17 @@
 @echo off
 
+::   http://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing,
+:: software distributed under the License is distributed on an
+:: "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+:: KIND, either express or implied. See the License for the
+:: specific language governing permissions and limitations
+:: under the License.
+::
+::
+:: Batch Convert PDF files into PDF/A files
+::
 :: This script in its current form will convert source PDF to PDF/A using Ghostscript. Ghostscript needs to be installed on the system.
 :: This script is a good candidate to do other batch PDF jobs like adding watermarks. Or to be rewritten in powershell, where it will be more readable too.
 
