7b151c33a100e7bb862b65ae0b2133c673a7b98a
Darren Dignam <darren.dignam@gmail.com>
2023-11-06 12:21:03 +0100

    Update PDF_batch_verify.bat
    Added a license to the file

--- Windows/veraPDF/PDF_batch_verify.bat
+++ Windows/veraPDF/PDF_batch_verify.bat
@@ -1,5 +1,17 @@
 @echo off
 
+::   http://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing,
+:: software distributed under the License is distributed on an
+:: "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+:: KIND, either express or implied. See the License for the
+:: specific language governing permissions and limitations
+:: under the License.
+::
+:: Batch process PDF files with veraPDF and pipe the resulting XML output into files on the flisesystem.
+
+
 :: needs the three parameters - otherwise show the help
 if [%1]==[] goto help
 if [%2]==[] goto help
